1d8b668432b1cba69234211a83e25529b104c1fd
nileshmantati <nileshmantati@gmail.com>
2025-07-31 19:04:16 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import ProductList from './components/ProductList';
 import Cart from './features/cart/Cart';
 import { useSelector } from 'react-redux';
